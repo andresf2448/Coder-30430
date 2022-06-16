4fc9292c312fc6db3b23ee91f3362ad4a8321f39
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-06-16 18:59:54 -0500

    Clase 1

---
+++ Clase 1/index.js
@@ -0,0 +1,50 @@
+// comentario de linea
+
+/* 
+comentario
+de
+bloque
+*/
+
+/* Alt + shift + A */
+/* ctrl + shift + A */
+
+/* //formas de declarar una variable recomendada
+let nombre = "Agostina";
+const EDAD = 15;
+
+//no recomendada
+var apellido = "Ruiz";
+
+console.log(typeof(edad)); */
+
+/* let numeroUno = 10;
+let numeroDos = 11;
+let resultado = numeroUno / numeroDos;
+const PRINCIPAL = 5;
+
+console.log(resultado + PRINCIPAL); */
+
+/* let nombre = "andres";
+let apellido = "ruiz";
+
+let resultado = nombre + "_" + apellido;
+console.log(resultado);
+
+let resta = nombre - apellido;
+
+console.log(resta); */
+
+/* const numero = 5;
+numero = 10; */
+
+/* console.log("3" + "5"); */
+
+/* let nombre = prompt("Ingrese el nombre");
+let saludo = "Hola " + nombre;
+
+alert(saludo); */
+
+let entrada = prompt("Ingresar una letra");
+let salida = entrada + " " + "ingresada";
+alert(salida);

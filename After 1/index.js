25596e1952bd01a52dfbb66f73c0119491ea11da
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-06-24 17:55:48 -0500

    after 1

---
+++ After 1/index.js
@@ -0,0 +1,28 @@
+/* let numero = "hola";
+
+if(!isNaN(numero)){
+  let result = numero + 2;
+} */
+
+/* let numero = parseInt(prompt("Ingrese un numero"));
+
+for (let i = 0; i < 5; i++) {
+  let result = numero + i;
+  let mensaje = `${numero} + ${i} = ${result}`;
+  alert(mensaje);
+} */
+
+/* for (let i = 0; i < 10; i++) {
+  if (i === 3) {
+    alert("Sala llena, intente mas tarde");
+    continue;
+  }
+
+  let nombre = prompt("Ingrese su nombre");
+  let mensaje = `Sala N ${i} Nombre: ${nombre}`;
+  alert(mensaje);
+} */
+
+let numero = prompt("ingrese un numero");
+
+let resultado = 1 + parseInt(numero);
